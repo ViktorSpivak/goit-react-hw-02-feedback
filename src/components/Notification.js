83e7c94e5ac1b0f3b4e8e38942af76ef5153c57d
Viktor Spivak <spivakmailbox@gmail.com>
2020-03-04 14:12:49 +0200

    second commit

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
-export default Notification = ({ message }) => (
+const Notification = ({ message }) => (
   <Fragment>
     <h2>{message}</h2>
   </Fragment>
 );
+export default Notification;
